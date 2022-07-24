cd4c4582cdc69e9c9eb2c2c7130e36a179714deb
Ahzam Shahnil <62467164+ahzam-shahnil@users.noreply.github.com>
2022-07-24 12:29:10 +0500

    Update constants.dart

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:logger/logger.dart';
-
-var logger = Logger();
 
 var kWidth = Get.size.width;
 var kHeight = Get.size.height;
